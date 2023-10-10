57fb208f4ae94f89fe2d53ac4564895a6ad76a59
LouisLeonardIMT <louis.leonard@etu.imt-nord-europe.fr>
2023-10-10 11:32:04 +0200

    siteproV3

---
+++ SiteProV3/template_header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+<title>Louis Léonard</title>
+<link rel="stylesheet" href="style.css" type="text/css"
+media="screen" title="default" charset="utf-8" />
+</head>
+<body>
---
+++ SiteProV3/template_menu.php
@@ -0,0 +1,20 @@
+<?php
+function renderMenuToHTML($currentPageId) {
+    // un tableau qui d\'efinit la structure du site
+    $mymenu = array(
+        // idPage titre
+        'index' => array( 'Accueil' ),
+        'cv' => array( 'CV' ),
+        'projets' => array('Mes Projets')
+    );
+    echo "<nav class ='menu'>\n<ul>\n";
+    foreach($mymenu as $pageId => $pageParameters) {
+        if ($pageId == $currentPageId) {
+            echo "<li><a href='".$pageId.".php' id='currentpage'>".$pageParameters[0]."</a></li>\n";
+        } else {
+            echo "<li><a href='".$pageId.".php'>".$pageParameters[0]."</a></li>\n";
+        }
+    }
+    echo "</ul>\n</nav>";
+}
+?>
