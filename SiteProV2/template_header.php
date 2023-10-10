d0740bfc5b25d538f4d4ef586de371de5e3c7e11
LouisLeonardIMT <louis.leonard@etu.imt-nord-europe.fr>
2023-10-10 11:25:26 +0200

    correction header

--- SiteProV2/template_header.php
+++ SiteProV2/template_header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title>Hector Durand</title>
+<title>Louis Léonard</title>
 <link rel="stylesheet" href="style.css" type="text/css"
 media="screen" title="default" charset="utf-8" />
 </head>
--- SiteProV2/template_menu.php
+++ SiteProV2/template_menu.php
@@ -9,7 +9,7 @@ function renderMenuToHTML($currentPageId) {
     );
     echo "<nav class ='menu'>\n<ul>\n";
     foreach($mymenu as $pageId => $pageParameters) {
-        if ($pageId === $currentPageId) {
+        if ($pageId == $currentPageId) {
             echo "<li><a href='".$pageId.".php' id='currentpage'>".$pageParameters[0]."</a></li>\n";
         } else {
             echo "<li><a href='".$pageId.".php'>".$pageParameters[0]."</a></li>\n";
